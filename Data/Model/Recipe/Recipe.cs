6a7dcbe9c05ae8fb9048dcee60b4078a2e5053fa
Максим <maxim@s-avt.ru>
2019-12-17 01:30:37 +0300

    Null values from DB to entities leads to errors

--- Data/Model/Recipe/Recipe.cs
+++ Data/Model/Recipe/Recipe.cs
@@ -5,18 +5,18 @@ namespace Data.Model
 {
     public class Recipe : Entity
     {
-        public string Name { get; set; }
-        public string SourceUrl { get; set; }
-        public string ImagePath { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? SourceUrl { get; set; }
+        public string? ImagePath { get; set; }
+        public string? Description { get; set; }
 
         public int Difficulty { get; set; }
         public int Rating { get; set; }
 
         public int PortionsCount { get; set; }
         public CalorieType CalorieType { get; set; }
-        public virtual List<IngredientsGroup> IngredientGroups { get; set; }
-        public virtual List<RecipeIngredient> Ingredients { get; set; }
-        public virtual List<RecipeTag> Tags { get; set; }
+        public virtual List<IngredientsGroup>? IngredientGroups { get; set; }
+        public virtual List<RecipeIngredient>? Ingredients { get; set; }
+        public virtual List<RecipeTag>? Tags { get; set; }
     }
 }
--- ServiceLayer/Projections/Recipe/RecipeSlim.cs
+++ ServiceLayer/Projections/Recipe/RecipeSlim.cs
@@ -4,8 +4,8 @@ namespace Cooking.ServiceLayer.Projections
 {
     public class RecipeSlim : RecipeProjection
     {
-        public string Name { get; set; }
-        public string ImagePath { get; set; }
+        public string? Name { get; set; }
+        public string? ImagePath { get; set; }
         public CalorieType CalorieType { get; set; }
     }
 }
