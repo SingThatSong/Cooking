39230b90b8e5f9a178b9dd0f92f5e55211466c8b
Maxim Grishin <maxim.grishin@lifeit.ru>
2019-12-18 19:07:16 +0300

    Addressing CA1053 - making Program class static

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace WebApplication1
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
