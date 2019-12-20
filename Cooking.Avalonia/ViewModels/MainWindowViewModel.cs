2dbe7402b314e4224668fcefd43078789bcbf483
Максим <maxim@s-avt.ru>
2019-12-21 01:54:40 +0300

    Avalonia project added

---
+++ Cooking.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Data.Model.Plan;
+using ServiceLayer;
+using System;
+
+namespace avatest.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+        public Week CurrentWeek => WeekService.GetWeekAsync(DateTime.Now).Result;
+    }
+}
--- Cooking/DTO/DayEdit.cs
+++ Cooking/DTO/DayEdit.cs
@@ -7,8 +7,6 @@ namespace Cooking.DTO
     {
         public new RecipeSelectDto? Dinner { get; set; }
         public new bool DinnerWasCooked { get; set; }
-#pragma warning disable CS0067
         public event PropertyChangedEventHandler? PropertyChanged;
-#pragma warning restore CS0067
     }
 }
