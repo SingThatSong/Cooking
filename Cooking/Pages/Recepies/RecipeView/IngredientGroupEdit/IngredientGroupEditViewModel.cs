ecc474f4ce215cbac7cf479a94198a666245d7cf
Maxim Grishin <maxim.grishin@lifeit.ru>
2019-12-27 19:30:06 +0300

    Validation crunch

--- Cooking/Pages/Recepies/RecipeView/IngredientGroupEdit/IngredientGroupEditViewModel.cs
+++ Cooking/Pages/Recepies/RecipeView/IngredientGroupEdit/IngredientGroupEditViewModel.cs
@@ -1,12 +1,27 @@
-﻿namespace Cooking.Pages
+﻿using Cooking.Commands;
+using System;
+
+namespace Cooking.Pages
 {
     public partial class IngredientGroupEditViewModel : OkCancelViewModel
     {
+        public DelegateCommand LoadedCommand { get; }
         public IngredientGroupEditViewModel(DialogService dialogService, DTO.IngredientGroupEdit? ingredientGroup = null) : base(dialogService)
         {
             IngredientGroup = ingredientGroup ?? new DTO.IngredientGroupEdit();
+            LoadedCommand = new DelegateCommand(OnLoaded);
         }
 
-        public DTO.IngredientGroupEdit IngredientGroup { get; }
+        // WARNING: this is a crunch
+        // When we open ingredient creation dialog second+ time, validation cannot see Ingredient being a required property, but when we change it's value - everything is ok
+        // There is no such behaviour when using navigation, so it seems it's something Mahapps-related
+        private void OnLoaded()
+        {
+            var backup = IngredientGroup;
+            IngredientGroup = null;
+            IngredientGroup = backup;
+        }
+
+        public DTO.IngredientGroupEdit IngredientGroup { get; private set; }
     }
 }
