43258b9361c60ba3362acc829100a4b00afd7ea1
Максим <maxim@s-avt.ru>
2020-01-04 21:13:11 +0300

    Ingredients list refactoring

--- Cooking/Pages/Ingredients/IngredientsViewModel.cs
+++ Cooking/Pages/Ingredients/IngredientsViewModel.cs
@@ -2,9 +2,7 @@
 using Cooking.Commands;
 using Cooking.DTO;
 using Cooking.Pages.Ingredients;
-using Cooking.ServiceLayer;
 using Data.Model;
-using MahApps.Metro.Controls.Dialogs;
 using Prism.Regions;
 using PropertyChanged;
 using ServiceLayer;
@@ -18,20 +16,22 @@ namespace Cooking.Pages
     [AddINotifyPropertyChangedInterface]
     public partial class IngredientsViewModel
     {
+        // Dependencies
         private readonly IRegionManager regionManager;
         private readonly DialogService dialogUtils;
         private readonly IngredientService ingredientService;
         private readonly IMapper mapper;
 
+        // State
         public ObservableCollection<IngredientEdit>? Ingredients { get; private set; }
         public bool IsEditing { get; set; }
 
-        public DelegateCommand AddIngredientCommand { get; }
-
-        public DelegateCommand<IngredientEdit> ViewIngredientCommand { get; }
-        public DelegateCommand<IngredientEdit> EditCategoryCommand { get; }
-        public DelegateCommand<Guid> DeleteCategoryCommand { get; }
+        // Commands
         public DelegateCommand LoadedCommand { get; }
+        public DelegateCommand AddIngredientCommand { get; }
+        public DelegateCommand<Guid> DeleteCategoryCommand { get; }
+        public DelegateCommand<IngredientEdit> ViewIngredientCommand { get; }
+        public DelegateCommand<IngredientEdit> EditIngredientCommand { get; }
 
         public IngredientsViewModel(IRegionManager regionManager, 
                                     DialogService dialogUtils, 
@@ -43,15 +43,23 @@ namespace Cooking.Pages
             Debug.Assert(ingredientService != null);
             Debug.Assert(mapper != null);
 
-            this.regionManager = regionManager;
-            this.dialogUtils = dialogUtils;
+            this.regionManager     = regionManager;
+            this.dialogUtils       = dialogUtils;
             this.ingredientService = ingredientService;
-            this.mapper = mapper;
-            LoadedCommand = new DelegateCommand(OnLoaded, executeOnce: true);
-            ViewIngredientCommand = new DelegateCommand<IngredientEdit>(ViewIngredient);
-            AddIngredientCommand = new DelegateCommand(AddRecipe);
-            DeleteCategoryCommand = new DelegateCommand<Guid>(DeleteIngredient);
-            EditCategoryCommand = new DelegateCommand<IngredientEdit>(EditIngredient);
+            this.mapper            = mapper;
+            LoadedCommand          = new DelegateCommand(OnLoaded, executeOnce: true);
+            AddIngredientCommand   = new DelegateCommand(AddIngredient);
+            DeleteCategoryCommand  = new DelegateCommand<Guid>(DeleteIngredient);
+            EditIngredientCommand  = new DelegateCommand<IngredientEdit>(EditIngredient);
+            ViewIngredientCommand  = new DelegateCommand<IngredientEdit>(ViewIngredient);
+        }
+
+        #region Top-level functions
+        private void OnLoaded()
+        {
+            Debug.WriteLine("IngredientsViewModel.OnLoaded");
+            var dataDb = ingredientService.GetProjected<IngredientEdit>(mapper);
+            Ingredients = new ObservableCollection<IngredientEdit>(dataDb);
         }
 
         private void ViewIngredient(IngredientEdit ingredient)
@@ -63,56 +71,45 @@ namespace Cooking.Pages
             regionManager.RequestNavigate(Consts.MainContentRegion, nameof(Recepies), parameters);
         }
 
-        private async void EditIngredient(IngredientEdit ingredient)
+        public async void DeleteIngredient(Guid recipeId)
+        {
+            await dialogUtils.ShowYesNoDialog("Точно удалить?", "Восстановить будет нельзя", successCallback: () => OnIngredientDeleted(recipeId))
+                             .ConfigureAwait(false);
+        }
+
+        public async void AddIngredient()
+        {
+            await dialogUtils.ShowOkCancelDialog<IngredientEditView, IngredientEditViewModel>("Новый ингредиент", successCallback: OnNewIngredientCreated)
+                             .ConfigureAwait(false);
+        }
+
+        public async void EditIngredient(IngredientEdit ingredient)
         {
             var viewModel = new IngredientEditViewModel(ingredientService, dialogUtils, mapper.Map<IngredientEdit>(ingredient));
-            await dialogUtils.ShowCustomMessageAsync<IngredientEditView, IngredientEditViewModel>("Редактирование ингредиента", viewModel).ConfigureAwait(false);
-            
-            if (viewModel.DialogResultOk)
-            {
-                await ingredientService.UpdateAsync(mapper.Map<Ingredient>(viewModel.Ingredient)).ConfigureAwait(false);
-                var existingRecipe = Ingredients.Single(x => x.ID == ingredient.ID);
-                mapper.Map(viewModel.Ingredient, existingRecipe);
-            }
+            await dialogUtils.ShowOkCancelDialog<IngredientEditView, IngredientEditViewModel>("Редактирование ингредиента", viewModel, successCallback: OnIngredientEdited)
+                             .ConfigureAwait(false);
         }
+        #endregion
 
-        private void OnLoaded()
+        #region Callbacks
+        private async void OnIngredientDeleted(Guid id)
         {
-            Debug.WriteLine("IngredientsViewModel.OnLoaded");
-            var dataDb = ingredientService.GetProjected<IngredientEdit>(mapper);
-            Ingredients = new ObservableCollection<IngredientEdit>(dataDb);
+            await ingredientService.DeleteAsync(id).ConfigureAwait(false);
+            Ingredients!.Remove(Ingredients.Single(x => x.ID == id));
         }
 
-        public async void DeleteIngredient(Guid id)
+        private async void OnIngredientEdited(IngredientEditViewModel viewModel)
         {
-            var result = await DialogCoordinator.Instance.ShowMessageAsync(
-                this, 
-                "Точно удалить?",
-                "Восстановить будет нельзя",
-                style: MessageDialogStyle.AffirmativeAndNegative,
-                settings: new MetroDialogSettings()
-                {
-                    AffirmativeButtonText = "Да",
-                    NegativeButtonText = "Нет"
-                }).ConfigureAwait(true);
-
-            if (result == MessageDialogResult.Affirmative)
-            {
-                await ingredientService.DeleteAsync(id).ConfigureAwait(true);
-                Ingredients!.Remove(Ingredients.Single(x => x.ID == id));
-            }
+            await ingredientService.UpdateAsync(mapper.Map<Ingredient>(viewModel.Ingredient)).ConfigureAwait(false);
+            var existing = Ingredients.Single(x => x.ID == viewModel.Ingredient.ID);
+            mapper.Map(viewModel.Ingredient, existing);
         }
 
-        public async void AddRecipe()
+        private async void OnNewIngredientCreated(IngredientEditViewModel viewModel)
         {
-            var viewModel = await dialogUtils.ShowCustomMessageAsync<IngredientEditView, IngredientEditViewModel>("Новый ингредиент").ConfigureAwait(false);
-
-            if (viewModel.DialogResultOk)
-            {
-                var id = await ingredientService.CreateAsync(mapper.Map<Ingredient>(viewModel.Ingredient)).ConfigureAwait(false);
-                viewModel.Ingredient.ID = id;
-                Ingredients!.Add(viewModel.Ingredient);
-            }
+            var id = await ingredientService.CreateAsync(mapper.Map<Ingredient>(viewModel.Ingredient)).ConfigureAwait(false);
+            Ingredients!.Add(viewModel.Ingredient);
         }
+        #endregion
     }
 }
