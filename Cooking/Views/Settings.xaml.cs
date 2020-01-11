be081a73b0e7d5b490d9da65faf7c480fc8ac052
Максим <maxim@s-avt.ru>
2020-01-11 04:05:53 +0300

    Settings edit view

---
+++ Cooking/Views/Settings.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Regions;
+using System.Globalization;
+using System.Windows.Controls;
+using WPFLocalizeExtension.Engine;
+
+namespace Cooking.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class Settings : UserControl, IRegionMemberLifetime
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        public bool KeepAlive => true;
+    }
+}
