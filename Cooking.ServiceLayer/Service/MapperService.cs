2b5f48b8edc3aa216e3ce9cf6da11eb5b3fa7cee
Максим <maxim@s-avt.ru>
2020-02-12 23:26:08 +0300

    Move equality comparison to Entity class

--- Cooking.ServiceLayer/Service/MapperService.cs
+++ Cooking.ServiceLayer/Service/MapperService.cs
@@ -29,15 +29,14 @@ namespace ServiceLayer
                 // Ignore Culture changes in mapping
                 // Why: projections should not load culture, so on update they will not know it. Keep Culture as it is in database.
                 cfg.CreateMap<Entity, Entity>()
-                   .ForMember(x => x.Culture, opts => opts.MapFrom((src, dest) => dest.Culture ?? src.Culture));
+                   .ForMember(x => x.Culture, opts => opts.MapFrom((src, dest) => dest.Culture ?? src.Culture))
+                   .EqualityComparison((a, b) => a.ID == b.ID);
 
                 cfg.CreateMap<IngredientsGroup, IngredientsGroup>()
-                   .IncludeBase<Entity, Entity>()
-                   .EqualityComparison((a, b) => a.ID == b.ID); // When mapping collections of values, use this to detect the same objects
+                   .IncludeBase<Entity, Entity>(); // When mapping collections of values, use this to detect the same objects
 
                 cfg.CreateMap<RecipeIngredient, RecipeIngredient>()
-                   .IncludeBase<Entity, Entity>()
-                   .EqualityComparison((a, b) => a.ID == b.ID);
+                   .IncludeBase<Entity, Entity>();
 
                 cfg.CreateMap<Tag, Tag>()
                    .ForMember(x => x.Recipies, opts => opts.Ignore()) // Ignore current relationships on tag data update
