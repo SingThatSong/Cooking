1c7b820cf6fca60feb60cc4afbb64ade96e652ab
Максим <maxim@s-avt.ru>
2020-07-10 02:39:12 +0300

    Propagate Entity mapping everywhere

--- Cooking.WPF/Services/Mapping/MapperService.cs
+++ Cooking.WPF/Services/Mapping/MapperService.cs
@@ -29,57 +29,47 @@ namespace Cooking
 
                     // Base mapping for db-dto mappings
                     cfg.CreateMap<Entity, Entity>()
-                       .EqualityComparison((a, b) => a.ID == b.ID);
+                       .EqualityComparison((a, b) => a.ID == b.ID)
+                       .IncludeAllDerived();
 
                     // Map created recipe to displayed in list
                     cfg.CreateMap<RecipeEdit, RecipeListViewDto>()
-                       .IncludeBase<Entity, Entity>()
 
                        // It is a new recipe, so we just set LastCooked to infinity
                        .ForMember(x => x.LastCooked, opts => opts.MapFrom(_ => int.MaxValue));
 
                     // Backup dto for editing
-                    cfg.CreateMap<GarnishEdit, GarnishEdit>()
-                       .IncludeBase<Entity, Entity>();
+                    cfg.CreateMap<GarnishEdit, GarnishEdit>();
 
-                    cfg.CreateMap<RecipeEdit, RecipeEdit>()
-                       .IncludeBase<Entity, Entity>();
+                    cfg.CreateMap<RecipeEdit, RecipeEdit>();
 
-                    cfg.CreateMap<TagEdit, TagEdit>()
-                       .IncludeBase<Entity, Entity>();
+                    cfg.CreateMap<TagEdit, TagEdit>();
 
-                    cfg.CreateMap<IngredientEdit, IngredientEdit>()
-                       .IncludeBase<Entity, Entity>();
+                    cfg.CreateMap<IngredientEdit, IngredientEdit>();
 
                     cfg.CreateMap<MeasureUnit, MeasureUnit>()
-                       .IncludeBase<Entity, Entity>()
                        .EqualityComparison((a, b) => a.ID == b.ID);
 
                     cfg.CreateMap<RecipeIngredient, RecipeIngredientEdit>()
                        .EqualityComparison((a, b) => a.ID == b.ID)
-                       .IncludeBase<Entity, Entity>()
                        .ReverseMap()
                        .EqualityComparison((a, b) => a.ID == b.ID)
                         // Do not map ingredient object, it's not new, so db will fail on attempt to create duplicate
                        .ForMember(x => x.Ingredient, opts => opts.Ignore())
                        .ForMember(x => x.MeasureUnit, opts => opts.Ignore())
-                       .ForMember(x => x.MeasureUnitGuid, opts => opts.MapFrom(x => x.MeasureUnit != null ? (Guid?)x.MeasureUnit.ID : null))
-                       .IncludeBase<Entity, Entity>();
+                       .ForMember(x => x.MeasureUnitID, opts => opts.MapFrom(x => x.MeasureUnit != null ? (Guid?)x.MeasureUnit.ID : null));
 
                     // Project Recipe from db to displayed in lists
                     cfg.CreateMap<Recipe, RecipeListViewDto>()
-                       .IncludeBase<Entity, Entity>()
                        .AfterMap<RecipeDtoConverter>();
 
                     // Project Recipe from db to displayed in recipe view
                     cfg.CreateMap<Recipe, RecipeEdit>()
-                       .IncludeBase<Entity, Entity>()
                        .ForMember(x => x.Tags, opt => opt.MapFrom(x => x.Tags.Select(t => t.Tag)))
                        .AfterMap<RecipeConverter>();
 
                     // Update db entry
                     cfg.CreateMap<RecipeEdit, Recipe>()
-                       .IncludeBase<Entity, Entity>()
                        .AfterMap((_, dest) =>
                        {
                            if (dest.Tags != null)
@@ -97,8 +87,7 @@ namespace Cooking
 
                     // Cleanup below
                     // -----------------------
-                    cfg.CreateMap<RecipeIngredientEdit, RecipeIngredientEdit>()
-                       .IncludeBase<Entity, Entity>();
+                    cfg.CreateMap<RecipeIngredientEdit, RecipeIngredientEdit>();
                     cfg.CreateMap<Garnish, GarnishEdit>().ReverseMap();
                     cfg.CreateMap<Day, DayEdit>();
                     cfg.CreateMap<Tag, TagEdit>();
