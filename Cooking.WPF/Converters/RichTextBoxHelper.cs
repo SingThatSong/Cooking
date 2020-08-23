68a3d9ab8b0ec383ab196f9b148a542acbd32cec
Максим <maxim@s-avt.ru>
2020-08-23 23:26:27 +0300

    RichTextBoxHelper refactoring

--- Cooking.WPF/Converters/RichTextBoxHelper.cs
+++ Cooking.WPF/Converters/RichTextBoxHelper.cs
@@ -17,13 +17,17 @@ namespace Cooking.WPF.Converters
     [SuppressMessage("Style", "CA1721", Justification = "Names are intended by AttachedProperty")]
     public sealed class RichTextBoxHelper
     {
+        private enum RichTextboxStatus
+        {
+            CanBeModified,
+            CanNotBeModified
+        }
+
         /// <summary>
         /// Gets or sets attached property to serve as a proxy for binding between RichTextBox's document and string in view model.
         /// </summary>
         [AttachedProperty(OnPropertyChanged = nameof(OnDocumentXamlChanged), Options = FrameworkPropertyMetadataOptions.BindsTwoWayByDefault)]
         public static string? DocumentXaml { get; set; } = string.Empty;
-
-        private static bool IsEditing { get; set; }
 
         /// <summary>
         /// Getter for AttachedProperty. Filled in with <see cref="Bindables"/>.
@@ -43,37 +47,37 @@ namespace Cooking.WPF.Converters
 
         private static void OnDocumentXamlChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
         {
-            if (IsEditing)
+            var richTextBox = (RichTextBox)dependencyObject;
+
+            if (richTextBox.Tag != null && (RichTextboxStatus)richTextBox.Tag != RichTextboxStatus.CanBeModified)
             {
                 return;
             }
 
-            var richTextBox = (RichTextBox)dependencyObject;
+            // Get document
+            string text = GetDocumentXaml(dependencyObject);
+            richTextBox.Document ??= new FlowDocument();
 
-            // Parse the XAML to a document (or use XamlReader.Parse())
-            string xaml = GetDocumentXaml(dependencyObject);
-            richTextBox.Document = new FlowDocument();
-
-            if (xaml != null)
+            if (text != null)
             {
                 // Set the document
-                using var stream = new MemoryStream(Encoding.UTF8.GetBytes(xaml));
+                using var stream = new MemoryStream(Encoding.UTF8.GetBytes(text));
                 var range = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
                 range.Load(stream, DataFormats.Rtf);
             }
 
             if (richTextBox.Tag == null)
             {
-                richTextBox.Tag = new object();
+                richTextBox.Tag = RichTextboxStatus.CanBeModified;
 
                 // When the document changes update the source
                 richTextBox.TextChanged += (obj2, e2) =>
                 {
-                    IsEditing = true;
+                    richTextBox.Tag = RichTextboxStatus.CanNotBeModified;
                     var buffer = new MemoryStream();
                     new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Save(buffer, DataFormats.Rtf);
                     SetDocumentXaml(richTextBox, Encoding.UTF8.GetString(buffer.ToArray()));
-                    IsEditing = false;
+                    richTextBox.Tag = RichTextboxStatus.CanBeModified;
                 };
             }
         }
