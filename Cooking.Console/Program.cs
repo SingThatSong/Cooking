bbc070a4f0670e30aea812ff5d74fa7133da6beb
Максим <maxim@s-avt.ru>
2020-11-09 02:06:01 +0300

    Add Console project

---
+++ Cooking.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cooking.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
